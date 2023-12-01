c66d751adc4adfe217584dc2195efde7f22c8657
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-12-01 10:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
             // Get the radius from the user using the prompt() function
             let radius = prompt("Enter the radius of the circle:");
             // write you code here and display the result to the user
-           
+	       const pi = Math.PI;
+           const areaOfCircle = (radius, pi) => {
+           return pi * radius * radius;
+};
+         alert("The area of the circle : " + areaOfCircle(radius, pi).toFixed(2));
+// function circle(radius)
+// {
+//     this.radius = radius;
+//   // area method
+//     this.area = function () 
+//     {
+//         return Math.PI * this.radius * this.radius;
+//     };
+  
+// }
+// var c = new circle(3);
+// alert('The area of the circle', c.area().toFixed(2));
 }
 calculateArea();
